a97ec1f1578211ca0678207bdbb2c1912ee691e4
wenbei <wenbei@wafersystems.com>
2023-08-09 15:10:49 +0800

    配置修噶

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = function (env) {
       index: path.join(__dirname, "../src/CustomBpmnViewer.js"),
     },
     output: {
-      path: path.join(__dirname, "./dist/"),
+      path: path.join(__dirname, "../dist/"),
       filename: "index.js",
       libraryTarget: 'umd', // 采用通用模块定义
       libraryExport: 'default', // 兼容 ES6 的模块系统、CommonJS 和 AMD 模块规范
